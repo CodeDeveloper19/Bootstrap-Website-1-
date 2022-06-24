0a43a1708eccb5429bacfe52ac472da084c449ae
Okoli Akachukwu <okoli555aka@gmail.com>
2022-06-24 10:52:54 +0100

    was able to make the header responsive

--- script.js
+++ script.js
@@ -1,22 +1,42 @@
+let isDateButtonOn;
+let isHeaderButtonOn;
+let counter1 = 0;
+let counter2 = 0;
+
 document.getElementById("date-button").addEventListener("click", () => {
-    let jmediaquery = window.matchMedia( "(max-width: 900px)" )
-    if (jmediaquery.matches && document.getElementById("header").classList[5] == "flex-row") {
-        document.getElementById("header").classList.replace("flex-row", "flex-column")
-        console.log(document.getElementById("date").classList)
+    counter1++;
+    let jmediaquery = window.matchMedia( "(max-width: 990px)" )
+    if (jmediaquery.matches && document.getElementById("header").classList[5] == "flex-row" && !isHeaderButtonOn) {
+        document.getElementById("header").classList.replace("flex-row", "flex-column");
+        isDateButtonOn = true;
     }
-    else if (jmediaquery.matches && document.getElementById("header").classList[5] == "flex-column") {
-        document.getElementById("header").classList.replace("flex-column", "flex-row")
-        console.log(document.getElementById("date").classList)
-}
+    else if (jmediaquery.matches && document.getElementById("header").classList[5] == "flex-column" && !isHeaderButtonOn) {
+        document.getElementById("header").classList.replace("flex-column", "flex-row");
+        isDateButtonOn = false;
+    }
+    else if (counter1 % 2 == 1) {
+        isDateButtonOn = true;
+    } 
+    else if (counter1 % 2 == 0) {
+        isDateButtonOn = false;
+    }
 })
 
-
 document.getElementById("navbar-header-button").addEventListener("click", () => {
-    let jmediaquery = window.matchMedia( "(max-width: 900px)" )
-    if (jmediaquery.matches && document.getElementById("header").classList[5] == "flex-row") {
-        document.getElementById("header").classList.replace("flex-row", "flex-column")
+    counter2++;
+    let jmediaquery = window.matchMedia( "(max-width: 990px)" )
+    if (jmediaquery.matches && document.getElementById("header").classList[5] == "flex-row" && !isDateButtonOn) {
+        document.getElementById("header").classList.replace("flex-row", "flex-column");
+        isHeaderButtonOn = true;
     }
-    else if (jmediaquery.matches && document.getElementById("header").classList[5] == "flex-column") {
-        document.getElementById("header").classList.replace("flex-column", "flex-row")
-}
+    else if (jmediaquery.matches && document.getElementById("header").classList[5] == "flex-column" && !isDateButtonOn) {
+        document.getElementById("header").classList.replace("flex-column", "flex-row");
+        isHeaderButtonOn = false;
+    }
+    else if (counter2 % 2 == 1) {
+        isHeaderButtonOn = true;
+    } 
+    else if (counter2 % 2 == 0) {
+        isHeaderButtonOn = false;
+    }
 })
